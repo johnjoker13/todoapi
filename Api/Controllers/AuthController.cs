63fa539e437740422eb6fb1674abef91eb4025cd
johnjoker13 <joaoavelino2008@hotmail.com>
2023-12-12 16:48:37 -0300

    feat: adds post method

--- Api/Controllers/AuthController.cs
+++ Api/Controllers/AuthController.cs
@@ -1,4 +1,8 @@
+using System.Net;
+using Application.Commands;
 using Contracts;
+using MapsterMapper;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Api.Controllers;
@@ -7,11 +11,23 @@ namespace Api.Controllers;
 [Route("api/v1/")]
 public class AuthController : ControllerBase
 {
+    private readonly ISender _mediator;
+    private readonly IMapper _mapper;
+
+    public AuthController(ISender mediator, IMapper mapper)
+    {
+        _mediator = mediator;
+        _mapper = mapper;
+    }
+
     [HttpPost]
     [Route("register")]
     public async Task<IActionResult> Register(RegisterRequest request)
     {
 
-        return Ok();
+        var command = _mapper.Map<RegisterCommand>(request);
+        var result = await _mediator.Send(command);
+
+        return StatusCode(StatusCodes.Status201Created, result);
     }
 }
