3a08a577dcb8826df3d2ce5a0012937768daed2d
johnjoker13 <joaoavelino2008@hotmail.com>
2023-12-12 16:47:45 -0300

    feat: creates validator

---
+++ Application/Users/Commands/RegisterCommandValidator.cs
@@ -0,0 +1,30 @@
+using FluentValidation;
+
+namespace Application.Commands;
+
+public class RegisterCommandValidator : AbstractValidator<RegisterCommand>
+{
+    public RegisterCommandValidator()
+    {
+        RuleFor(x => x.Name)
+            .NotEmpty()
+            .MinimumLength(2)
+            .MaximumLength(50);
+
+        RuleFor(x => x.LastName)
+            .NotEmpty()
+            .MinimumLength(2)
+            .MaximumLength(50);
+
+        RuleFor(x => x.Email)
+            .NotEmpty()
+            .EmailAddress()
+            .MaximumLength(150);
+
+        RuleFor(x => x.Password)
+            .NotEmpty()
+            .MinimumLength(8)
+            .MaximumLength(16)
+            .Matches("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[^\\da-zA-Z]).{8,16}$");
+    }
+}
