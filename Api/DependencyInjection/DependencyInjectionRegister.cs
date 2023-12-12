a126b47f09155e873f252ae37ef1d073d8a5ae36
johnjoker13 <joaoavelino2008@hotmail.com>
2023-12-12 20:27:35 -0300

    refactor: removes swashbuckle

--- Api/DependencyInjection/DependencyInjectionRegister.cs
+++ Api/DependencyInjection/DependencyInjectionRegister.cs
@@ -1,13 +1,13 @@
+using Api.Common;
+
 namespace Api.DependencyInjection;
 
 public static class DependencyInjectionRegister
 {
     public static IServiceCollection AddPresentantion(this IServiceCollection services)
     {
-        services
-            .AddSwaggerGen()
-            .AddEndpointsApiExplorer()
-            .AddControllers();
+        services.AddControllers();
+        services.AddMappings();
 
         return services;
     }
